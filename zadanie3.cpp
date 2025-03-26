b90d0f5e424a378893d852137fd31d21336570d6
marcinn07 <167750735+marcinn07@users.noreply.github.com>
2025-03-26 13:25:13 +0100

    Add files via upload

---
+++ zadanie3.cpp
@@ -0,0 +1,28 @@
+﻿﻿#include <iostream>
+
+using namespace std;
+
+class przeliczanie {
+public:
+    int kilowaty;
+    int kmh;
+    void obliczanie() {
+        kmh = kilowaty * 1, 36;
+        cout << kilowaty << " kilowatow to " << kmh << " km/h" << endl;
+    }
+
+};
+
+int main()
+{
+
+    int kilowaty;
+    cout << "Podaj ilosc kilowaty: ";
+    cin >> kilowaty;
+    ;
+    przeliczanie kmh;
+
+    kmh.kilowaty = kilowaty;
+
+    kmh.obliczanie();
+}
