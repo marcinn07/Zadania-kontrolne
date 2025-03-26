e410c8477fcc36b62b49a27d0183d3641bedcb60
marcinn07 <167750735+marcinn07@users.noreply.github.com>
2025-03-26 13:01:39 +0100

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+class figura {       
+public:             
+    int a,b,h;
+    int obj,pole, dlkraw;
+    void objetosc() {
+        obj = a * b * h;
+        cout << "V=" << obj << "cm^3" << endl;
+    }
+    void polepow() {
+        pole = (2 * a * b) + (2 * b * h) + (2 * a * h);
+        cout << "Pp=" << pole << "cm^2" << endl;
+    }
+    void krawedzie() {
+        dlkraw = (4 * a ) + (4 * b ) + (4 * h);
+        cout << "Dl.krawedzi=" << dlkraw << "cm" << endl;
+    }
+};
+
+
+int main()
+{
+
+    int a, b, h;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    cout << "Podaj h: ";
+    cin >> h;
+
+    figura prostopadloscian;
+
+    prostopadloscian.a = a;
+    prostopadloscian.b = b;
+    prostopadloscian.h = h;
+
+    prostopadloscian.objetosc();
+    prostopadloscian.polepow();
+    prostopadloscian.krawedzie();
+}
---
+++ zadanie2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class przeliczanie {
+public:
+    int m;
+    int mile;
+    void obliczanie() {
+        mile = m * 0.00062137;
+        cout << m << " metrow to " << mile << " mil" << endl;
+    }
+
+};
+
+int main()
+{
+
+    int m;
+    cout << "Podaj ilosc metrow: ";
+    cin >> m;
+    ;
+    przeliczanie mile;
+
+    mile.m = m;
+
+    mile.obliczanie();
+}
